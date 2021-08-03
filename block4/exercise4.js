74d852bed935af8ede9fcb326793d822552b7957
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-04 00:22:00 +0200

    JS_conditional_stringchecker_comma_question

--- block4/exercise4.js
+++ block4/exercise4.js
@@ -1,8 +1,27 @@
-
-
-module.exports = {
-    checker
+function checker(string) {
+  var commaArr = string.split(",");
+  var questionArr = string.split("?");
+  let commaCount = commaArr.length - 1;
+  let questionCount = questionArr.length - 1;
+  if (commaCount > 1) {
+    if (questionCount > 1) {
+      var sentence = `${commaCount} commas, ${questionCount} question marks`;
+      return sentence;
+    } else {
+      var sentence_two = `${commaCount} commas, ${questionCount} question mark`;
+      return sentence_two;
+    }
+  } else {
+    if (questionCount > 1) {
+      var sentence_three = `${commaCount} comma, ${questionCount} question marks`;
+      return sentence_three;
+    } else {
+      var sentence_four = `${commaCount} comma, ${questionCount} question mark`;
+      return sentence_four;
+    }
+  }
 }
 
-
-
+module.exports = {
+  checker,
+};
