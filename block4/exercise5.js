45b640a0117a105fd6d7c526a3c687aaf1a8c412
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-04 01:07:34 +0200

    JS_conditional_counting capitals

--- block4/exercise5.js
+++ block4/exercise5.js
@@ -1,3 +1,15 @@
-module.exports = {
-    howManyCaps
+function howManyCaps(string) {
+  let capArr = [];
+  var arr = string.replace(/\s+/g, "").split("");
+  arr.forEach((element) => {
+    if (element === element.toUpperCase()) {
+      capArr.push(element);
+    }
+  });
+  var count = capArr.length;
+  var message = `There are ${count} capitals and these are ${capArr.join(",")}`;
+  return message;
 }
+module.exports = {
+  howManyCaps,
+};
