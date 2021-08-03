6265ff05f7ce1c8b51b321fca5819d59ca2f4083
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-03 17:27:13 +0200

    js_conditional_block4_ex1

--- block4/exercise1.js
+++ block4/exercise1.js
@@ -1,4 +1,10 @@
-
-    module.exports = {
-    check_who_is_older
+function check_who_is_older(name, age, name2, age2) {
+  if (age > age2) {
+    return `${name} age ${age} is older than ${name2} age ${age2}`;
+  } else {
+    return `${name2} age ${age2} is older than ${name} age ${age}`;
+  }
 }
+module.exports = {
+  check_who_is_older,
+};
