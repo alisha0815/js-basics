843ffc0e7f9b17c157370500495906d95b9bbd3e
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-03 17:19:05 +0200

    JS_04_00_conditional

--- block4/exercise0.js
+++ block4/exercise0.js
@@ -1,4 +1,16 @@
-
-module.exports = {
-    isTrue
+function isTrue(data) {
+  if (
+    !data == NaN ||
+    data == "" ||
+    data == false ||
+    data == null ||
+    data === 0
+  ) {
+    return false;
+  } else {
+    return true;
+  }
 }
+module.exports = {
+  isTrue,
+};
