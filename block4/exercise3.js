2059da369c8f85a8cd4ec0167d97bf3065fff756
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-03 22:32:28 +0200

    js_conditional_block4_ex3

--- block4/exercise3.js
+++ block4/exercise3.js
@@ -1,5 +1,14 @@
-
-
-module.exports ={
-    check_types
+function check_types(arr) {
+  let count = [];
+  for (let i = 0; i < arr.length; i++) {
+    let dataType = typeof arr[i];
+    if (!count.includes(dataType)) {
+      count.push(dataType);
+    }
+  }
+  return count.length;
 }
+
+module.exports = {
+  check_types,
+};
