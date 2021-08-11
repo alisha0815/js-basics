b0e8dfa868c8eac178cd16583b3a535268668145
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-11 14:09:35 +0200

    js loop exercise unique element extended version

--- block4/exercise9.js
+++ block4/exercise9.js
@@ -1,5 +1,28 @@
-
+function uniqueElements(arr, max) {
+  let newArr = [];
+  let uniqueArr = new Map();
+  arr.forEach((name) => {
+    if (!uniqueArr[name]) {
+      uniqueArr[name] = true;
+      if (typeof name === "number" && name >= max) {
+        newArr.push(name);
+      }
+    }
+  });
+  return `old array ${arr.join(",")}, new array ${newArr.join(",")}`;
+}
 
 module.exports = {
-    uniqueElements
-}
+  uniqueElements,
+};
+
+// Define a function called 'uniqueElements' which takes an array and a number ('max') as arguments.
+// Inside it create an array called 'newArr'
+// Loop inside the array (argument) and for each iteration check if the element is
+//  not present in the 'newArr',
+
+// if it is a number,
+// and that the element is bigger or equal to 'max', if all is ok push it in.
+
+// Make up the sentence as required using the 'newArr' array.
+// Return the sentence.
