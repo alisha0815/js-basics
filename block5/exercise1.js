f08bc96a5b033cf6831cfe2562e4c57c593b5764
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-11 16:26:33 +0200

    js functions refresh calculator

--- block5/exercise1.js
+++ block5/exercise1.js
@@ -1,3 +1,29 @@
-module.exports = {
-    calc
+function calc(firstNum, secondNum, operator) {
+  if (operator === "+") {
+    return firstNum + secondNum;
+  }
+  if (operator === "-") {
+    return firstNum - secondNum;
+  }
+  if (operator === "*") {
+    return firstNum * secondNum;
+  }
+  if (operator === "/") {
+    return firstNum / secondNum;
+  } else {
+    return "wrong data provided";
+  }
 }
+module.exports = {
+  calc,
+};
+
+// Define a function called 'calc' which takes 3 arguments.
+// Set up a condition for each of the possible 4 signs (+, -, *, /);
+// Before running any of these check that all arguments are there, if any is missing return 'wrong data provided'
+// Otherwise do the proper operation based on the sign.
+// Return the result.
+
+//======================  EXAMPLE  ========================
+calc(5, 15, "+");
+20; // <======  EXPECTED OUTPUT
