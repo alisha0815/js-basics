b874c0daff5a7a0e7057dc87d712d324571f5e37
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-11 17:14:55 +0200

    js functions filter

--- block5/exercise3.js
+++ block5/exercise3.js
@@ -1,5 +1,21 @@
+function filter(arr, type, min) {
+  let newArr = [];
+  arr.forEach((item) => {
+    var itemTyp = typeof item;
+    if (itemTyp !== type && item.length >= min) {
+      newArr.push(item);
+    }
+  });
+  return newArr;
+}
 
+// Define a function called 'filter' which takes 3 arguments: an array, a type and a min-length.
+// inside it create a variable called 'newArr' (an empty array)
+// use a loop and for each iteration check that the element is not of the type passed as argument
+// and that it's length is greater or equal to the min-length passed.
+// if it matches these criteria push it in 'newArr'.
+// return 'newArr'.
 
 module.exports = {
-    filter
-}
+  filter,
+};
