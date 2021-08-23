5f8b4f95ade6d62bd45e9c1849eacb5b6d58fdf1
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-23 16:54:28 +0200

    js object function

--- block6/exercise3.js
+++ block6/exercise3.js
@@ -1,6 +1,10 @@
-
-
-
-module.exports ={
-    obj, modifyObject
+let obj = {};
+function modifyObject(obj, key, value) {
+  obj[key] = value;
+  return obj;
 }
+
+module.exports = {
+  obj,
+  modifyObject,
+};
