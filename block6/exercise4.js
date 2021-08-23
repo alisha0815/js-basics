c3b9b0a425b2b3515277894a819ce3e0e03e8ad0
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-23 17:12:12 +0200

    js object refresh

--- block6/exercise4.js
+++ block6/exercise4.js
@@ -1,4 +1,23 @@
+function addToList(arrMovie) {
+  let moviesDB = [];
 
-module.exports = {
-    addToList
+  arrMovie.forEach((movie) => {
+    let movieObj = {};
+
+    movieObj["title"] = movie;
+    movieObj["id"] = arrMovie.indexOf(movie);
+    moviesDB.push(movieObj);
+  });
+  return moviesDB;
 }
+module.exports = {
+  addToList,
+};
+
+// define a function called 'addToList' which takes an array as argument.
+// inside it define a variable called 'moviesDB' as an empty array.
+// loop inside the array (argument) and each time the loop runs create a new object
+// assign it a key of "title" and the value of the array element,
+// assign it a key of "id" and get the value from the loop current index.
+// push the object to the 'moviesDB' array.
+// outside the loop return 'moviesDB'.
