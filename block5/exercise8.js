72fa0bbb47b71b1ddb4d3d20367e746336abb286
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-23 13:48:30 +0200

    string cutter function js

--- block5/exercise8.js
+++ block5/exercise8.js
@@ -1,52 +1,15 @@
-function isNum(element) {
-  if (!isNaN(element)) {
-    return "no need for conversion";
-  }
-}
-
-function numberConverter(arr) {
-  let numbers = [];
-  let unconvertable = [];
-  if (arr.every(isNum)) {
-  } else {
-  }
-}
-
-if (arr.every((element) => !isNaN(element))) {
-  return "no need for conversion";
-} else {
-  arr.forEach((item) => {
-    if (!isNaN(parseInt(item))) {
-      var strNum = parseInt(item);
-      numbers.push(strNum);
-    } else {
-      unconvertable.push(item);
+function strCut(string, indOne, indTwo) {
+  arr = string.split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (i == indOne) {
+      arr.splice(i, 1);
     }
-    return `${numbers.length} were converted to numbers: ${numbers.join(
-      ","
-    )}, ${unconvertable.length} couldn't be converted`;
-  });
+    if (i == indTwo - 1) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr.join("");
 }
-// == HINT ===
-//
-// You should use isNaN and typeof.
-//
-//
-// == STEPS ===
-//
-// Define a function called 'numberConverter' which takes an array as argument.
-// Inside it define two arrays, 'numbers' and 'unconvertable'.
-// Loop through the array (argument) and for each element first check if it is not already a number
-// Then check if it is not NaN, in which case push it to the 'numbers' array, otherwise push it to the 'unconvertable' array.
-// Make up the sentence as required using the arrays.
-// Return the sentence.
-//
-
-// numberConverter(["12", "55", "hello"]);
-// ("2 were converted to numbers: 12,55, 1 couldn't be converted"); // <======  EXPECTED OUTPUT
-// numberConverter([22, 55, 66]);
-// ("no need for conversion"); // <======  EXPECTED OUTPUT
-//====================
 module.exports = {
   strCut,
 };
