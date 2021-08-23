11b4f09b12c9527b2c6d127898049d52514ea59d
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-23 17:45:34 +0200

    js swap function

--- block6/exercise5.js
+++ block6/exercise5.js
@@ -1,4 +1,24 @@
-
-module.exports= {
-    swap
+function swap(obj) {
+  let newObj = {};
+  for (var item in obj) {
+    let newKeys = obj[item];
+    let newValues = item;
+    newObj[newKeys] = newValues;
+  }
+  return newObj;
 }
+module.exports = {
+  swap,
+};
+
+// create a function called 'swap' which takes an object as argument.
+
+// inside it define a new object called 'newObj'.
+
+// loop inside the object (argument)
+
+// for each iteration
+// –add the object (argument) key as the 'newObj' value
+// –add the the object (argument) value as the 'newObj' key
+
+// outside the loop return 'newObj'
