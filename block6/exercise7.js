87bbf54412fac1dc33aa90ab8692b4339dfbf45f
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-24 13:23:50 +0200

    run on range function js object

--- block6/exercise7.js
+++ block6/exercise7.js
@@ -1,4 +1,23 @@
+function runOnRange(obj) {
+  let arr = [];
+  var startNum = obj.start;
+  var endNum = obj.end;
+  var stepNum = obj.step;
+  if (obj.step == undefiend) {
+    return arr;
+  }
+  if (startNum > endNum && stepNum < 1) {
+    for (var i = startNum; i >= endNum; i += stepNum) {
+      arr.push(i);
+    }
+  } else {
+    for (var i = startNum; i <= endNum; i += stepNum) {
+      arr.push(i);
+    }
+  }
 
-module.exports = {
-    runOnRange
+  return arr;
 }
+module.exports = {
+  runOnRange,
+};
