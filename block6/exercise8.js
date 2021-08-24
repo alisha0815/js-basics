53622c2ec07e292db7cf9b6550e39e1efccf8d66
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-24 16:18:20 +0200

    last function in progress

--- block6/exercise8.js
+++ block6/exercise8.js
@@ -1,4 +1,19 @@
+var obj = { a: 20, b: 26, c: 0 };
 
-module.exports ={
-    last
+function last(obj) {
+  for (var item in obj) {
+    var newObj = {};
+    // the array of keys of the object
+    var keyArr = Object.keys(item);
+    var lastKey = keyArr.pop();
+
+    // the array of values of the object
+    var valueArr = Object.values(item);
+    var lastValue = valueArr.pop();
+    newObj[lastKey] = lastValue;
+  }
+  return newObj;
 }
+module.exports = {
+  last,
+};
