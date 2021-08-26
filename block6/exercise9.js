d8d7e48a62d6f953c43158906e82372caeb2bbe0
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-26 16:06:08 +0200

    sum all function in progress

--- block6/exercise9.js
+++ block6/exercise9.js
@@ -1,4 +1,21 @@
-
-module.exports = {
-    sumAll
+function sumAll(obj) {
+  if (Object.values(obj) === 0 || obj === null) {
+    return 0;
+  } else {
+    var sum = 0;
+    for (var num in obj) {
+      sum += obj[num];
+    }
+    return sum;
+  }
 }
+module.exports = {
+  sumAll,
+};
+
+// Define a function called 'sumAll' which takes an object as argument.
+// Then do some checks:
+// if the object is empty return 0;
+// if the object has only one value then return that.
+// if the object has more then you can loop and sum them.
+// return the sum.
