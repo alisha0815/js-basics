508c070ac6eb5e38b1b9f84d8b6b7d184b4029d9
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-26 22:01:46 +0200

    model object function

--- block6/exercise11.js
+++ block6/exercise11.js
@@ -1,4 +1,18 @@
-
-module.exports = {
-    model
+function model(action, obj, schema) {
+  var DB = [];
+  let newObj = {};
+  if (action !== "add") {
+    return "missing action argument or wrong provided";
+  }
+  var objArr = Object.keys(obj);
+  for (var item in schema) {
+    if (objArr.includes(item) && typeof obj[item] == schema[item]) {
+      newObj[item] = obj[item];
+    }
+  }
+  DB.push(newObj);
+  return DB;
 }
+module.exports = {
+  model,
+};
