4e58fe1a24538fd5d74f8942ad903371c30bcaca
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-27 12:49:28 +0200

    bankaccount function in progress

--- block6/exercise13.js
+++ block6/exercise13.js
@@ -1,5 +1,35 @@
+var total = 0;
+var bankAccount = {
+  total: new Number(),
+  withdraw: function (amount) {
+    total -= amount;
+    return total;
+  },
 
+  deposit: function (amount) {
+    total += amount;
+    return total;
+  },
+
+  balance: function () {
+    return total;
+  },
+};
 
 module.exports = {
-    bankAccount
-}
+  bankAccount,
+};
+
+// Total is a number and balance a method.
+
+// Define an object called 'bankAccount'.
+
+// define a key total with a value of 0;
+
+// define the following 3 methods as the keys of the object 'bankAccount', their values would be functions performing the withdraw/deposit/balance operations:
+
+// define a method called withdraw which takes an argument and subtracts it from the value of total
+
+// define a method called deposit which takes an argument and adds it to the value of total
+
+// define a method called balance which takes no arguments and just returns total
