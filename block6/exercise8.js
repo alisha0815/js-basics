bcc50d247303a5bb30e5cf60dcadf749fffc6672
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-27 17:51:05 +0200

    last funcdtion completed

--- block6/exercise8.js
+++ block6/exercise8.js
@@ -1,19 +1,26 @@
 var obj = { a: 20, b: 26, c: 0 };
 
 function last(obj) {
-  for (var item in obj) {
-    var newObj = {};
-    // the array of keys of the object
-    var keyArr = Object.keys(item);
-    var lastKey = keyArr.pop();
+  // for (var item in obj) {
+  var newObj = {};
+  // the array of keys of the object
+  var keyArr = Object.keys(obj);
+  var lastKey = keyArr.pop();
 
-    // the array of values of the object
-    var valueArr = Object.values(item);
-    var lastValue = valueArr.pop();
-    newObj[lastKey] = lastValue;
-  }
-  return newObj;
+  // the array of values of the object
+  var valueArr = Object.values(obj);
+  var lastValue = valueArr.pop();
+  // newObj[lastKey] = lastValue;
+  // }
+  return { [lastKey]: lastValue };
 }
 module.exports = {
   last,
 };
+
+//======================  EXAMPLE  ========================
+// var obj = {a: 20, b: 26, c: 0}
+// last(obj)
+// {c:0} // <======  EXPECTED OUTPUT
+//{a: 20, b: 26, c: 0}  <====== obj
+//=========================================================
