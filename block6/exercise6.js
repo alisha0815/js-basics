071b2c620aa49ed3a3253e5bd894fa026b86ca67
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-27 16:51:38 +0200

    no changes

--- block6/exercise6.js
+++ block6/exercise6.js
@@ -1,24 +1,23 @@
-var arrObj = [{ name: "Antonello" }, { name: "George" }, { name: "Golda" }];
-
-function getIndex(arrObj, key, value) {
-  arrObj.forEach((obj) => {
-    for (var name in obj) {
-      if (name == key && obj[name] == value) {
-        break;
-      }
+// var arr = [{ name: "Antonello" }, { name: "George" }, { name: "Golda" }];
+function getIndex(arr, key, value) {
+  let index = -1;
+  arr.forEach((item, i) => {
+    if (item[key] === value) {
+      index = i;
     }
-    return arrObj.indexOf(obj);
   });
+  return index;
 }
 module.exports = {
   getIndex,
 };
 
-// Declare a function  called 'getIndex' which takes 3 arguments: an array, a key and a value.
-// Declare a variable called 'index' and give it a value of -1.
-
-// Using a 'for' loop, loop through the array and for each iteration check if the element at the passed key has the passed value.
-
-// If you find a match set 'index' to the current index in the loop and break from the loop using the keyword 'break';
-
-// Outside the loop return 'index'.
+// const getIndex = (arr, key, val) => {
+//   let idx = -1;
+//   arr.forEach((item, i) => {
+//     if (item[key] === val) {
+//       idx = i;
+//     }
+//   });
+//   return idx;
+// };
