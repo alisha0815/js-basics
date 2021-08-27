287c8fac8ad2ece4bc3cc25e2d280eaba372e675
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-27 12:02:48 +0200

    model function in progress

--- block6/exercise12.js
+++ block6/exercise12.js
@@ -1,6 +1,29 @@
-
-
-
-module.exports = {
-	model
+function model(action, obj, schema) {
+  var DB = [];
+  let newObj = {};
+  if (action !== "add") {
+    return "missing action argument or wrong provided";
+  }
+  var objArr = Object.keys(obj);
+  for (var item in schema) {
+    if (objArr.includes(item)) {
+      if (typeof obj[item] == schema.item.type) {
+        newObj[item] = obj[item];
+      } else {
+        if (objArr.includes(item) && item.default !== undefined) {
+          neweObj[item] = item.default;
+        }
+      }
+    }
+  }
+  DB.push(newObj);
+  return DB;
 }
+module.exports = {
+  model,
+};
+
+// Then loop through the schema and for each iteration check if the value is not present in the 'newObj' and that is present in the default.
+// In this case add the default to the 'newObj'.
+// Then outside the loop push our 'newObj' to the 'DB' array.
+// Return the array.
