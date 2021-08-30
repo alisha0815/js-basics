f22f920c9e55c33eb64e69259a2979cc2928bfda
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-30 14:10:20 +0200

    splice function js object in progress

--- block6/exercise14.js
+++ block6/exercise14.js
@@ -1,4 +1,26 @@
+var obj = { a: 1, b: 2, c: 2 };
+console.log(obj);
+function splice(obj, start, end) {
+  var end = end || 1;
+  var arr = Object.entries(obj);
+  // [ [ 'a', 1 ], [ 'b', 2 ], [ 'c', 2 ] ]
 
-module.exports ={
-	splice
+  var newObj = {};
+  let count = 1;
+  for (var key in obj) {
+    count++;
+
+    if (start <= arr.length - 1 && end > 0) {
+      newObj[key] = obj[key];
+    }
+    if (start > arr.length - 1 || end === 0) {
+    }
+  }
+  return newObj;
 }
+// {"a", 1} if start <= arr.length - 1
+// var result = splice(obj, 0, 2);
+
+module.exports = {
+  splice,
+};
