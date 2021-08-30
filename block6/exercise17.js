ea035f71b37685730b5e0a40356f2334baeb0a64
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-30 17:21:20 +0200

    biggestnumber object function js

--- block6/exercise17.js
+++ block6/exercise17.js
@@ -1,4 +1,18 @@
+var obj = { a: 1, b: 20, c: 3, d: 4, e: 1, f: 4 };
+// console.log(obj);
 
-module.exports ={
-	biggestNumber
+const objNumeticallyDescending = (array) => array.sort((a, b) => b[1] - a[1]);
+var results = objNumeticallyDescending(Object.entries(obj));
+console.log(results[0]);
+
+function biggestNumber(obj) {
+  let sorted = objNumeticallyDescending(Object.entries(obj));
+  let newObj = {};
+  newObj["num"] = sorted[0][1];
+  newObj["key"] = sorted[0][0];
+  return newObj;
 }
+
+module.exports = {
+  biggestNumber,
+};
