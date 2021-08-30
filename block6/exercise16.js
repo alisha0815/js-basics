c1533b8b6ee6c387258c064403bafea626e3a1e0
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-30 16:42:04 +0200

    object sort keys and values function

--- block6/exercise16.js
+++ block6/exercise16.js
@@ -1,4 +1,43 @@
+var obj = { a: 1, b: 20, c: 3, d: 4, e: 1, f: 4 };
+console.log(obj);
 
-module.exports ={
-    sort
+const arrToObj = (arr) => Object.fromEntries(arr);
+
+const sortObjKeysAlphabetically = (obj) =>
+  Object.fromEntries(Object.entries(obj).sort());
+
+// console.log(sortObjKeysAlphabetically(obj));
+// console.log(Object.entries(obj).sort().reverse());
+
+const sortAlphabeticallyDescending = (obj) =>
+  Object.fromEntries(Object.entries(obj).sort().reverse());
+
+function sort(obj, criterion, order) {
+  var arrObj = Object.entries(obj);
+  console.log(arrObj);
+  if (criterion === undefined || order === undefined) {
+    return "missing argument here!";
+  } else if (criterion !== undefined && order !== undefined) {
+    if (criterion == "values") {
+      if (order == "ascending") {
+        let resultOne = arrObj.sort((a, b) => a[1] - b[1]);
+        return arrToObj(resultOne);
+      } else if (order == "descending") {
+        let resultTwo = arrObj.sort((a, b) => b[1] - a[1]);
+        return arrToObj(resultTwo);
+      }
+    }
+    if (criterion == "keys") {
+      if (order == "ascending") {
+        return sortObjKeysAlphabetically(obj);
+      } else if ((order = "descending")) {
+        return sortAlphabeticallyDescending(obj);
+      }
+    }
+  }
 }
+
+console.log(sort(obj, "values", "ascending"));
+module.exports = {
+  sort,
+};
