e002ea849e94400424b9c115a388b62b2199b713
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-30 23:56:48 +0200

    moviesDB function nested loops object js_in progress

--- block7/exercise1.js
+++ block7/exercise1.js
@@ -1,6 +1,51 @@
+var DB = [
+  {
+    genre: "thriller",
+    movies: [
+      {
+        title: "the usual suspects",
+        release_date: 1999,
+      },
+    ],
+  },
+  {
+    genre: "comedy",
+    movies: [
+      {
+        title: "pinapple express",
+        release_date: 2008,
+      },
+    ],
+  },
+];
 
+var obj2 = { title: "the usual suspects" };
 
+// console.log(DB);
 
-module.exports ={
-	moviesDB
-}
+const moviesDB = (arr, str, obj) => {
+  arr.forEach(function (array) {
+    console.log(array);
+    if (array.genre == str) {
+      console.log(array.genre);
+      if (array.movies) {
+        console.log(array.movies);
+        array.movies.forEach(function (ele) {
+          console.log(ele);
+          if (ele.title == obj.title) {
+            console.log(
+              "the movie the usual suspects is already in the database"
+            );
+          } else {
+            console.log(obj);
+          }
+        });
+      }
+    }
+  });
+};
+
+moviesDB(DB);
+module.exports = {
+  moviesDB,
+};
