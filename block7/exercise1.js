344016d62ce5144fb04e5317431921d2196a3dff
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-08-31 17:30:23 +0200

    movie database find index function object js completed

--- block7/exercise1.js
+++ block7/exercise1.js
@@ -19,33 +19,46 @@ var DB = [
   },
 ];
 
-var obj2 = { title: "the usual suspects" };
+var minariTitle = { title: "Minari" };
 
-// console.log(DB);
+const moviesDB = (array, genre, movieObj) => {
+  // Declare a function called 'moviesDB' which takes 3 arguments: an array(DB), a genre and a movie.
+  console.log(DB[0].genre === "thriller"); // Check the genre in the array
+  var index = array.findIndex((movie) => movie.genre === genre);
+  if (index === -1) {
+    //if genre is not given in the array
+    // Push the an object with the genre and an array called movies which contain the movie(argument).
+    console.log(minariTitle.title);
+    array.push({ genre: genre, movies: [{ title: movieObj.title }] });
+    return array;
+  }
+  var index2 = array[index].movies.findIndex(
+    (ele) => ele.title === movieObj.title
+  );
+  if (index2 === -1) {
+    array[index].movies.push(movieObj);
+  } else {
+    return `the movie ${movieObj.title} is already in the database!`;
+  }
+  return array;
 
-const moviesDB = (arr, str, obj) => {
-  arr.forEach(function (array) {
-    console.log(array);
-    if (array.genre == str) {
-      console.log(array.genre);
-      if (array.movies) {
-        console.log(array.movies);
-        array.movies.forEach(function (ele) {
-          console.log(ele);
-          if (ele.title == obj.title) {
-            console.log(
-              "the movie the usual suspects is already in the database"
-            );
-          } else {
-            console.log(obj);
-          }
-        });
-      }
-    }
-  });
+  // If it is present:
+  // At the index of the genre, Use Array.prototype.findIndex to check if the movie is there:
+  console.log(DB[0].movies[0].title);
 };
 
-moviesDB(DB);
+moviesDB;
+// console.log(moviesDB(DB, "thriller", obj2));
 module.exports = {
   moviesDB,
 };
+
+// If is not there:
+
+// push it in at that index.
+
+// If is there already:
+
+// return the movie ${THE TITLE OF THE MOVIE} is already in the database!`
+
+// at the end return DB.
