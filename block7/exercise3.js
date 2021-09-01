3035ec223e1df3acf47b9142fe89f1b36ab99adf
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-01 15:26:31 +0200

    constructor function js

--- block7/exercise3.js
+++ block7/exercise3.js
@@ -1,4 +1,17 @@
-
-module.exports = {
-    BankAccount
+class BankAccount {
+  constructor(amount = 0) {
+    this.total = amount;
+  }
+  deposit(value) {
+    this.total += value;
+  }
+  withdraw(value) {
+    this.total -= value;
+  }
+  balance() {
+    return this.total;
+  }
 }
+module.exports = {
+  BankAccount,
+};
