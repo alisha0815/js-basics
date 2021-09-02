4d79d3ad6adc51625c3c8cbc754de5bac0781f51
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-09-02 09:52:09 +0200

    constructor js functions in progress

--- block7/exercise4.js
+++ block7/exercise4.js
@@ -1,4 +1,41 @@
+// Create a constructor function called 'Universe' which takes two arguments
+// an initial value and the destination object (matter or energy).
+
+class Universe {
+  constructor(value, destination) {
+    // Declare a vairable "self", give it this as the value
+    // var self = value;
+    if (destination === "matter") {
+      this.matter.total = value;
+    } else if (destination === "energy") {
+      this.energy.total = 0;
+    }
+  }
+  deposit(value) {
+    this.matter.total -= value;
+    this.energy.total += value;
+  }
+  create(value) {
+    this.matter.total += value;
+    this.energy.total -= value;
+  }
+}
 
 module.exports = {
-    Universe
-}
+  Universe,
+};
+
+// == HINT ===
+// You could assign this to a variable to access it where is not available.
+
+// == STEPS ===
+
+// Create a method called 'create' which takes one argument, a value, and adds it to the 'total' of 'matter' and subtracts it from the 'total' of 'energy'
+
+// Using the keyword "this" create an object called 'energy':
+
+// in it give it a property 'total' and give it a value of or either the initial value passed as argument or 0, depending on weather the destination object was 'matter' or 'energy'.
+
+// Create a method called 'destroy' which takes one argument, a value, and subtract it form the 'total' of 'energy' and adds it on the 'total' of 'matter'.
+
+// Create a method called 'create' which takes one argument, a value, and adds it to the 'total' of 'energy' and subtracts it from the 'total' of 'matter'
